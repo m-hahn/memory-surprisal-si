c6067f02585c5767e0e9b400297cfd6f5d40ce00
Michael Hahn <mhahn29@gmail.com>
2020-02-14 10:30:57 -0800

    code

---
+++ writeup/getFiles.sh
@@ -0,0 +1,8 @@
+cp -r ~/memory-surprisal/writeup/tables .
+cp ~/memory-surprisal/writeup/neural/figures/* neural/figures/
+cp ~/memory-surprisal/writeup/ngrams/figures/* ngrams/figures/
+cp ~/memory-surprisal/writeup/control-ngrams.tex .
+cp ~/memory-surprisal/writeup/../code/analyze_ngrams/visualize/figures/* ../code/analyze_ngrams/visualize/figures/
+cp ~/memory-surprisal/writeup/si.tex .
+cp ~/memory-surprisal/writeup/literature.bib .
+
