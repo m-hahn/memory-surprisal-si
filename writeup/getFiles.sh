9dfc7a34f34e289e1c9e960651fece62fc19334b
Michael Hahn <mhahn29@gmail.com>
2020-09-11 12:51:05 +0800

    code

--- writeup/getFiles.sh
+++ writeup/getFiles.sh
@@ -1,18 +1,17 @@
 cp -r ~/memory-surprisal/writeup/tables .
 cp control-ngrams.tex ~/memory-surprisal/writeup/
-cp ~/memory-surprisal/writeup/../code/analyze_ngrams/visualize/figures/*MEDIANS_only* ../code/analyze_ngrams/visualize/figures/
 cp ~/memory-surprisal/writeup/results-table-pcfg.pdf .
 
+cp ~/memory-surprisal/code/ngram-control/analyze_ngrams/visualize/figures/*MEDIANS_only* ../code/analyze_ngrams/visualize/figures/
 
-
-cp ~/memory-surprisal/code/visualize_neural/figures/surprisal-branching-entropy-REAL.pdf figures/
-cp ~/memory-surprisal/code/visualize_neural/figures/full-GROUND-listener-surprisal-memory-HIST_z_byMem_onlyWordForms_boundedVocab.pdf figures/
+#cp ~/memory-surprisal/code/visualize_neural/figures/surprisal-branching-entropy-REAL.pdf figures/
+#cp ~/memory-surprisal/code/visualize_neural/figures/full-GROUND-listener-surprisal-memory-HIST_z_byMem_onlyWordForms_boundedVocab.pdf figures/
 cp ~/memory-surprisal/code/order-freedom/infostruc/analysis/visualize_neural/figures/Czech-PDT-listener-surprisal-memory-MEDIANS_onlyWordForms_boundedVocab.pdf figures/
 cp ~/memory-surprisal/writeup/quantiles-table.pdf .
 cp ~/memory-surprisal/writeup/auc-table.pdf .
 cp ~/memory-surprisal/writeup/it-table.pdf .
-cp ~/memory-surprisal/code/ngram-control/create_models_ngrams/morph/Japanese/tradeoffs/figures/* figures/
-cp ~/memory-surprisal/code/ngram-control/create_models_ngrams/morph/Sesotho/tradeoffs/figures/* figures/
+#cp ~/memory-surprisal/code/ngram-control/create_models_ngrams/morph/Japanese/tradeoffs/figures/* figures/
+#cp ~/memory-surprisal/code/ngram-control/create_models_ngrams/morph/Sesotho/tradeoffs/figures/* figures/
 
 cp ~/memory-surprisal/code/study1/figures/decay.pdf figures/
 cp ~/memory-surprisal/code/study1/figures/memory.pdf figures/
